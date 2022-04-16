3c42f373bcf22abd9577fb1f394a105ff1d70461
Davi <davi.barbosa2806@gmail.com>
2022-04-16 19:31:21 -0300

    Estrutura inicial da API

---
+++ src/main/kotlin/com/kotlinapirest/KotlinApiRestApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinapirest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinApiRestApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinapirest/KotlinApiRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinapirest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApiRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
